f657cbbb8f3eb239db695ba6db404537fd1e6d96
adam3497 <amador19972012@gmail.com>
2022-10-22 16:37:06 -0600

    Adding Spring Boot project

---
+++ src/main/java/com/qaproject/qa2022buscadordecontenido/Qa2022BuscadorDeContenidoApplication.java
@@ -0,0 +1,13 @@
+package com.qaproject.qa2022buscadordecontenido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qa2022BuscadorDeContenidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qa2022BuscadorDeContenidoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qaproject/qa2022buscadordecontenido/Qa2022BuscadorDeContenidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaproject.qa2022buscadordecontenido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qa2022BuscadorDeContenidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
