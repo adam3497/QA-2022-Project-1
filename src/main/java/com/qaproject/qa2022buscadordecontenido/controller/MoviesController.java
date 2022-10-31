1c3aa14b6ee734bbafaf2342e1c58189a34418ff
adam3497 <amador19972012@gmail.com>
2022-10-30 22:23:45 -0600

    (WIP) Movie page with different types of movie categories

--- src/main/java/com/qaproject/qa2022buscadordecontenido/controller/MoviesController.java
+++ src/main/java/com/qaproject/qa2022buscadordecontenido/controller/MoviesController.java
@@ -1,7 +1,6 @@
 package com.qaproject.qa2022buscadordecontenido.controller;
 
 import com.qaproject.qa2022buscadordecontenido.models.MovieResults;
-import com.qaproject.qa2022buscadordecontenido.models.TvShowResults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,24 +11,19 @@ import org.springframework.web.servlet.ModelAndView;
 import static com.qaproject.qa2022buscadordecontenido.utils.Constants.*;
 
 @RestController
-public class IndexController {
+public class MoviesController {
 
-    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+    private static final Logger log = LoggerFactory.getLogger(MoviesController.class);
 
-    @GetMapping("/home")
-    public ModelAndView index(RestTemplate restTemplate) {
-        String trendingShowsURL = TRENDING_TV_URL + TRENDING_DAY + API_KEY_URL;
+    @GetMapping("/movies")
+    public ModelAndView movies(RestTemplate restTemplate) {
         String trendingMoviesURL = TRENDING_MOVIE_URL + TRENDING_DAY + API_KEY_URL;
-        log.info(trendingShowsURL);
         log.info(trendingMoviesURL);
         try {
-            TvShowResults showResults = restTemplate.getForObject(trendingShowsURL, TvShowResults.class);
             MovieResults movieResults = restTemplate.getForObject(trendingMoviesURL, MovieResults.class);
             ModelAndView mv = new ModelAndView();
-            assert showResults != null;
             assert movieResults != null;
-            mv.addObject("shows", showResults.getResults());
-            mv.addObject("movies", movieResults.getResults());
+            mv.addObject("trendingMovies", movieResults.getResults(5));
             return mv;
         } catch (Exception e) {
             e.printStackTrace();
