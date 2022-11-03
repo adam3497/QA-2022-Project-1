bcacb9428d68cb2f338670095cad7331584967a7
adam3497 <amador19972012@gmail.com>
2022-11-03 10:29:21 -0600

    All tests for the IndexController class done

--- src/test/java/com/qaproject/qa2022buscadordecontenido/controller/IndexControllerTest.java
+++ src/test/java/com/qaproject/qa2022buscadordecontenido/controller/IndexControllerTest.java
@@ -1,5 +1,8 @@
 package com.qaproject.qa2022buscadordecontenido.controller;
 
+import com.qaproject.qa2022buscadordecontenido.models.MovieModel;
+import com.qaproject.qa2022buscadordecontenido.models.TvShowModel;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,12 +11,15 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 import static com.qaproject.qa2022buscadordecontenido.utils.Constants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Epic("IndexController Tests")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IndexControllerTest {
     @LocalServerPort
@@ -22,24 +28,39 @@ class IndexControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    IndexController indexController;
+
+    @Story("User types http://localhost:8080/home in the search bar on the browser and gets the home page of the application")
+    @Description("This method tests the functionality of the app to access the home page and get a OK status code")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     void indexStatusCode() throws Exception {
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL("http://localhost:" + port + "/home").toString(), String.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    @Story("The application should be able to connect to an external API and get responses according to the requests")
+    @Description("This method tests the status of the TMDB API according to the trending tv shows request")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     void apiTrendingShowResponseStatus() throws Exception{
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL(TRENDING_TV_URL + TRENDING_DAY + API_KEY_URL).toString(), String.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    @Story("The application should be able to connect to an external API and get responses according to the requests")
+    @Description("This method tests the status of the TMDB API according to the trending movies request")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     void apiTrendingMovieResponseStatus() throws Exception{
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL(TRENDING_MOVIE_URL + TRENDING_DAY + API_KEY_URL).toString(), String.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    @Story("The application should be able to connect to an external API and get responses according to the requests")
+    @Description("This method tests that the trending tv shows response from the TMDB API has an application/json type header")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     void apiTrendingShowContentTypeVerification() throws Exception {
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL(TRENDING_TV_URL + TRENDING_DAY + API_KEY_URL).toString(), String.class);
@@ -48,6 +69,9 @@ class IndexControllerTest {
         assertEquals("application/json;charset=utf-8", contentType.toString());
     }
 
+    @Story("The application should be able to connect to an external API and get responses according to the requests")
+    @Description("This method tests that the trending movies response from the TMDB API has an application/json type header")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     void apiTrendingMovieContentTypeVerification() throws Exception {
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL(TRENDING_MOVIE_URL + TRENDING_DAY + API_KEY_URL).toString(), String.class);
@@ -56,6 +80,9 @@ class IndexControllerTest {
         assertEquals("application/json;charset=utf-8", contentType.toString());
     }
 
+    @Story("The application should be able to connect to an external API and get responses according to the requests")
+    @Description("This method tests that the trending tv shows response has the right body content")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     void apiTrendingShowBodyContent() throws Exception {
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL(TRENDING_TV_URL + TRENDING_DAY + API_KEY_URL).toString(), String.class);
@@ -78,6 +105,9 @@ class IndexControllerTest {
                 () -> assertTrue(bodyContent.contains("first_air_date")));
     }
 
+    @Story("The application should be able to connect to an external API and get responses according to the requests")
+    @Description("This method tests that the trending movies response has the right body content")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     void apiTrendingMovieBodyContent() throws Exception {
         ResponseEntity<String> response = testRestTemplate.getForEntity(new URL(TRENDING_MOVIE_URL + TRENDING_DAY + API_KEY_URL).toString(), String.class);
@@ -99,5 +129,70 @@ class IndexControllerTest {
                 () -> assertTrue(bodyContent.contains("original_title")),
                 () -> assertTrue(bodyContent.contains("release_date")),
                 () -> assertTrue(bodyContent.contains("video")));
+    }
+
+    @Story("The application should be able to handle the responses and pass them to the HTML")
+    @Description("This method tests that the result provided by the index method from the indexController is a ModelAndView object" +
+            " and has the right content in it")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    void indexMethodModelAndViewResultVerification() {
+        assertAll("Index results",
+                () -> assertEquals(ModelAndView.class, indexController.index(testRestTemplate.getRestTemplate()).getClass()),
+                () -> assertEquals(2, indexController.index(testRestTemplate.getRestTemplate()).getModel().size()),
+                () -> assertTrue(indexController.index(testRestTemplate.getRestTemplate()).getModel().containsKey("movies")),
+                () -> assertTrue(indexController.index(testRestTemplate.getRestTemplate()).getModel().containsKey("shows")),
+                () -> assertEquals(ArrayList.class, indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies").getClass()),
+                () -> assertEquals(ArrayList.class, indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows").getClass()),
+                () -> assertTrue(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).size() > 1),
+                () -> assertTrue(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).size() > 1));
+    }
+
+    @Story("The application should be able to handle the responses and pass them to the HTML")
+    @Description("This method tests that the movie array that has to be in the ModelAndView object returned by the index() method " +
+            "should has some data")
+    @Severity(SeverityLevel.NORMAL)
+    @Test
+    void checkIfMovieResultsObjectsHasNonNullNorEmptyEntries() {
+        assertAll("MovieModel array from ModelAndView index()",
+                () -> assertFalse(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getTitle().isEmpty()),
+                () -> assertFalse(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getBackdrop_path().isEmpty()),
+                () -> assertFalse(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getMedia_type().isEmpty()),
+                () -> assertTrue(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getId() != 0),
+                () -> assertFalse(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getOverview().isEmpty()),
+                () -> assertTrue(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getVote_average() != 0.0f),
+                () -> assertFalse(((ArrayList<MovieModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("movies")).get(0).getRelease_date().isEmpty()));
+    }
+
+    @Story("The application should be able to handle the responses and pass them to the HTML")
+    @Description("This method tests that the tv show array that has to be in the ModelAndView object returned by the index() method " +
+            "should has some data")
+    @Severity(SeverityLevel.NORMAL)
+    @Test
+    void checkIfShowResultsObjectsHasNonNullNorEmptyEntries() {
+            assertAll("TvShowModel array from ModelAndView index()",
+                () -> assertFalse(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getName().isEmpty()),
+                () -> assertFalse(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getBackdrop_path().isEmpty()),
+                () -> assertFalse(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getMedia_type().isEmpty()),
+                () -> assertTrue(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getId() != 0),
+                () -> assertFalse(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getOverview().isEmpty()),
+                () -> assertTrue(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getVote_average() != 0.0f),
+                () -> assertFalse(((ArrayList<TvShowModel>)
+                        indexController.index(testRestTemplate.getRestTemplate()).getModel().get("shows")).get(0).getFirst_air_date().isEmpty()));
     }
 }
