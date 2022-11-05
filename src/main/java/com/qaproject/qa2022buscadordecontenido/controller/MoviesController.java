33d015785de6685c57dad04f5afda7c5058e7970
adam3497 <amador19972012@gmail.com>
2022-11-05 10:21:18 -0600

    Internal documentation for IndexController.java, MediaObject.java and MoviesController.java files

--- src/main/java/com/qaproject/qa2022buscadordecontenido/controller/MoviesController.java
+++ src/main/java/com/qaproject/qa2022buscadordecontenido/controller/MoviesController.java
@@ -10,19 +10,34 @@ import org.springframework.web.servlet.ModelAndView;
 
 import static com.qaproject.qa2022buscadordecontenido.utils.Constants.*;
 
+/**
+ * This class controls the whole interaction of /movies page and gives the data it needs
+ * @author adam3497
+ */
 @RestController
 public class MoviesController {
 
     private static final Logger log = LoggerFactory.getLogger(MoviesController.class);
 
+    /**
+     * This method is called when access http://localhost:8080/movies, it makes the request to the TMDB API
+     * To get the trending movies of the day, and it returns them as an attribute of a ModelAndView object
+     * @param restTemplate object of the current application
+     * @return ModelAndView object with one attribute called "trendingMovies"
+     */
     @GetMapping("/movies")
     public ModelAndView movies(RestTemplate restTemplate) {
+        // We create a new URL that will be used to get the trending movies
         String trendingMoviesURL = TRENDING_MOVIE_URL + TRENDING_DAY + API_KEY_URL;
-        log.info(trendingMoviesURL);
+        log.info("Requesting data to: " + trendingMoviesURL);
         try {
+            // We use the RestTemplate class to make a GET request and get the response in JSON format
+            // Spring Boot automatically parse the JSON response into an object of type MovieResults using Jackson library
             MovieResults movieResults = restTemplate.getForObject(trendingMoviesURL, MovieResults.class);
+            // Finally, we return a ModelAndView object with our list of movies as an attribute called "trendingMovies"
             ModelAndView mv = new ModelAndView();
             assert movieResults != null;
+            // In this case we are using just 5 movies instead of the whole result
             mv.addObject("trendingMovies", movieResults.getResults(5));
             return mv;
         } catch (Exception e) {
